7c76a10b7064b249132fe3e7c07f9c6e14c17dd0
Chanchal <chanchalrajput228@gmail.com>
2024-11-24 23:41:13 +0530

    Tokens

---
+++ src/models/user.model.js
@@ -0,0 +1,87 @@
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullName:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true
+    },
+    avatar:{
+        type:String, //cloudinary url is used 
+        required:true,
+    },
+    coverImage:{
+        type:String
+    },
+    watchHistory:[
+    {
+        type:Schema.Types.ObjectId,
+        ref:"Video"
+    }
+],
+    password:{
+        type:String,
+        required:[true,'Password is required']
+    },
+    refreshToken:{
+        type:String
+    }
+},{timestamps:true})
+
+// hum userSchema.pre("save",()=>{}) yha pr arrow function use nhi krskta hai kyuki isma context mallom hona bahoot jaroori hota hai jo ki arrow fucntion mai nhi pta hota jiski vajah sa normal async function liya hai jissa this ki help sa context ka pta chl ska or async isliya kyuki yeah time lega password encrypt krna mai
+
+// yha saltRounds = 10 diya hai
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")) return next();
+    this.password = bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+}
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+)
+}
+
+export const User = mongoose.model("User",userSchema)
