6426d4e43faab580dafcdab8fd42238c45196453
Chanchal <chanchalrajput228@gmail.com>
2024-12-02 00:34:22 +0530

    updation

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -263,7 +263,7 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json(200,req.user,"Current user fetched successfully")
+    .json(new ApiResponse(200,req.user,"Current user fetched successfully"))
 })
 
 const updateAccountDetails = asyncHandler(async(req,res)=>{
@@ -271,7 +271,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
     if(!fullName || !email){
         throw new ApiError(400,"All fields are required")
     }
-    const user = User.findByIdAndUpdate(req.user?._id,
+    const user = await User.findByIdAndUpdate(req.user?._id,
         {
           $set:{
             fullName:fullName,
@@ -283,7 +283,7 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
 
     return res
     .status(200)
-    .json(new ApiResponse(200,'user',"Account details updated successfully"))
+    .json(new ApiResponse(200,user,"Account details updated successfully"))
 })
 
 const updateUserAvatar = asyncHandler(async(req,res)=>{
